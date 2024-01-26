80e7e606ef765c4381d14c9ff96f3c248e22cfdb
Elias Hovdenes <email@example.com>
2024-01-26 11:21:48 +0100

    Rendering a md file in the leftbox

--- src/Components/LeftBoxContent.tsx
+++ src/Components/LeftBoxContent.tsx
@@ -1,15 +1,14 @@
-import {
-  Grid,
-  Paper,
-  Typography,
-} from '@mui/material';
-
+import { Grid, Paper, Typography } from "@mui/material";
+import RenderMarkdown from "./RenderLeftbox";
 
 const LeftBoxContent = () => {
   return (
     <Grid item xs={6}>
-      <Paper elevation={3} style={{ padding: '20px', overflowY: 'auto', height: '70vh'}}>
-        <Typography variant="body1" align="left">Here goes meeting notes</Typography>
+      <Paper
+        elevation={3}
+        style={{ padding: "20px", overflowY: "auto", height: "70vh" }}
+      >
+        <RenderMarkdown />
       </Paper>
     </Grid>
   );
