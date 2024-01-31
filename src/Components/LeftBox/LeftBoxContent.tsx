36be53f34bd2006006f7f0ecac5124630e9bcbec
Casper Karlsen <casperbkarlsen@gmail.com>
2024-01-31 13:21:27 +0100

    Cleaned up. still have some errors

--- src/Components/LeftBox/LeftBoxContent.tsx
+++ src/Components/LeftBox/LeftBoxContent.tsx
@@ -5,14 +5,14 @@ interface Props {
   link: string | null;
 }
 
-const LeftBoxContent = ({link}: Props) => {
+const LeftBoxContent = ({ link }: Props) => {
   return (
     <Grid item xs={6}>
       <Paper
         elevation={3}
         style={{ padding: "20px", overflowY: "auto", height: "70vh" }}
       >
-        <RenderMarkdown link = {link}/>
+        <RenderMarkdown link={link} />
       </Paper>
     </Grid>
   );
