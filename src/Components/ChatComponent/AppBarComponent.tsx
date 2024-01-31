f6614f3fd2cedea29e6938327ce43e5a2fcc56a7
Casper Karlsen <casperbkarlsen@gmail.com>
2024-01-31 11:36:55 +0100

    Split chat component

---
+++ src/Components/ChatComponent/AppBarComponent.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { AppBar, Toolbar, InputBase, Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+interface AppBarComponentProps {
+  input: string;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSendMessage: () => void;
+  handleClearMessages: () => void;
+}
+
+const AppBarComponent: React.FC<AppBarComponentProps> = ({ input, handleInputChange, handleSendMessage, handleClearMessages }) => {
+  return (
+    <AppBar position="static" style={{ background: "white", borderRadius: "20px", padding: "10px", marginLeft: "10%", width: "90%" }}>
+      <Toolbar>
+        <InputBase placeholder="What do you want to know?" inputProps={{ "aria-label": "type your message" }} value={input} onChange={handleInputChange} style={{ borderRadius: "20px", padding: "10px", color: "black", flex: 1 }} />
+        <Button variant="contained" color="primary" onClick={handleSendMessage}>
+          <SearchIcon></SearchIcon>
+        </Button>
+        <Button variant="outlined" color="secondary" onClick={handleClearMessages}>
+          Clear
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppBarComponent;
