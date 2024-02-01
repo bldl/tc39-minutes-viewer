0ffa6663b9b33490a5e00451566dd86d83ff907c
Brage Aasen <brageaasen@hotmail.com>
2024-02-01 17:59:30 +0100

    Folder and file navigation is now fully functional. The application fills the hash table on run.

--- src/Components/NavBar/FetchMeetings.tsx
+++ src/Components/NavBar/FetchMeetings.tsx
@@ -2,8 +2,6 @@ export const fetchHashTable = async () => {
     const basePath = 'public/meetings/'; // Hard-coded base path
     try {
       const response = await window.electronAPI.fetchHashTable(basePath);
-      console.log("Test");
-      console.log(response);
       return response;
     } catch (error) {
       console.error('Failed to fetch hash table:', error);
