c7dedd18863a284cc97b19e4ce2a3229890fa678
Casper Karlsen <casperbkarlsen@gmail.com>
2024-02-02 12:39:57 +0100

    prettiet alt

--- src/Components/LeftBox/LeftBoxContent.tsx
+++ src/Components/LeftBox/LeftBoxContent.tsx
@@ -10,7 +10,12 @@ const LeftBoxContent = ({ link }: Props) => {
     <Grid item xs={6}>
       <Paper
         elevation={3}
-        style={{ padding: "20px", overflowY: "auto", height: "77vh", width: "37.8vw",   }}
+        style={{
+          padding: "20px",
+          overflowY: "auto",
+          height: "77vh",
+          width: "37.8vw",
+        }}
       >
         <RenderMarkdown link={link} />
       </Paper>
