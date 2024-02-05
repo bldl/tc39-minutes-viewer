811900d6f3633fbfc7aec04eacb2fc3e66d10f49
brageaasen <brageaasen@hotmail.com>
2024-02-05 11:24:15 +0100

    Navbar is now functional with proper tree structure, year month day.

--- src/Components/NavBar/NavBarComponent.tsx
+++ src/Components/NavBar/NavBarComponent.tsx
@@ -10,83 +10,100 @@ import {
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 
+// Update the interface to match the new structure
 interface NavBarComponentProps {
-  folderOptions: { label: string; value: string }[];
-  fileOptions: Record<string, { label: string; value: string }[]>; // Record of folder path to array of files
-  label: string;
-  onSelectFolder: (value: string | null) => void;
-  onSelectFile: (value: string | null) => void;
+  hashTable: Record<string, Record<string, Record<string, string>>>;
+  onSelectYear: (year: string) => void;
+  onSelectMonth: (year: string, month: string) => void;
+  onSelectDay: (filePath: string) => void;
 }
 
 const NavBarComponent: React.FC<NavBarComponentProps> = ({
-  folderOptions,
-  fileOptions,
-  label,
-  onSelectFolder,
-  onSelectFile,
+  hashTable,
+  onSelectYear,
+  onSelectMonth,
+  onSelectDay,
 }) => {
+  const [openYears, setOpenYears] = useState<Record<string, boolean>>({});
+  const [openMonths, setOpenMonths] = useState<Record<string, boolean>>({});
 
-  const [openFolders, setOpenFolders] = useState<Record<string, boolean>>({});
-
-  const handleFolderClick = (folder: string) => {
-    setOpenFolders((prevOpenFolders) => ({
-      ...prevOpenFolders,
-      [folder]: !prevOpenFolders[folder],
+  const handleYearClick = (year: string) => {
+    setOpenYears((prevOpenYears) => ({
+      ...prevOpenYears,
+      [year]: !prevOpenYears[year],
     }));
-    onSelectFolder(folder);
+    onSelectYear(year);
   };
 
-  const handleFileClick = (filePath: string) => {
-    onSelectFile(filePath);
+  const handleMonthClick = (year: string, month: string) => {
+    setOpenMonths((prevOpenMonths) => ({
+      ...prevOpenMonths,
+      [month]: !prevOpenMonths[month],
+    }));
+    onSelectMonth(year, month);
   };
-
 
   return (
-    <Grid item xs={1} style={{ position: "relative" }}>
-        <Paper
-          elevation={3}
-          style={{
-            padding: "20px",
-            overflowY: "auto",
-            height: "89vh",
-            width: "15%",
-            position: "absolute",
-            top: "10%",
-            borderRadius: "20px"
-          }}
-        >
-          <List>
-            {folderOptions.map((folder) => (
-              <React.Fragment key={folder.value}>
-                <ListItem
-                  button
-                  onClick={() => handleFolderClick(folder.value)}
-                >
-                  <ListItemText primary={folder.label} />
-                  {openFolders[folder.value] ? <ExpandLess /> : <ExpandMore />}
-                </ListItem>
-                <Collapse
-                  in={openFolders[folder.value]}
-                  timeout="auto"
-                  unmountOnExit
-                >
-                  <List component="div" disablePadding>
-                    {fileOptions[folder.value]?.map((file) => (
+    <Grid item xs={12} style={{ position: "relative" }}>
+      <Paper
+        elevation={3}
+        style={{
+          padding: "20px",
+          overflowY: "auto",
+          height: "89vh",
+          width: "15%",
+          position: "absolute",
+          top: "10%",
+          borderRadius: "20px",
+        }}
+      >
+        <List>
+          {Object.keys(hashTable).map((year) => (
+            <React.Fragment key={year}>
+              <ListItem button onClick={() => handleYearClick(year)}>
+                <ListItemText primary={year} />
+                {openYears[year] ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={openYears[year]} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  {Object.keys(hashTable[year]).map((month) => (
+                    <React.Fragment key={month}>
                       <ListItem
-                        key={file.value}
                         button
-                        onClick={() => handleFileClick(file.value)}
-                        style={{ paddingLeft: "32px" }}
+                        onClick={() => handleMonthClick(year, month)}
+                        style={{ paddingLeft: 32 }}
                       >
-                        <ListItemText primary={file.label} />
+                        <ListItemText primary={month} />
+                        {openMonths[month] ? <ExpandLess /> : <ExpandMore />}
                       </ListItem>
-                    ))}
-                  </List>
-                </Collapse>
-              </React.Fragment>
-            ))}
-          </List>
-        </Paper>
+                      <Collapse
+                        in={openMonths[month]}
+                        timeout="auto"
+                        unmountOnExit
+                      >
+                        <List component="div" disablePadding>
+                          {Object.keys(hashTable[year][month]).map((day) => (
+                            <ListItem
+                              button
+                              onClick={() =>
+                                onSelectDay(hashTable[year][month][day])
+                              }
+                              style={{ paddingLeft: 64 }}
+                              key={day}
+                            >
+                              <ListItemText primary={day} />
+                            </ListItem>
+                          ))}
+                        </List>
+                      </Collapse>
+                    </React.Fragment>
+                  ))}
+                </List>
+              </Collapse>
+            </React.Fragment>
+          ))}
+        </List>
+      </Paper>
     </Grid>
   );
 };
