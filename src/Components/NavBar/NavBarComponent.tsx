807a87c149b3301a8858361ea23f76143f12bdad
Brage Aasen <brageaasen@Brages-Air.home>
2024-02-22 11:12:30 +0100

    Got box error, commiting before destructive actions

--- src/Components/NavBar/NavBarComponent.tsx
+++ src/Components/NavBar/NavBarComponent.tsx
@@ -1,21 +1,72 @@
-import React, { useState } from "react";
-import {
-  Grid,
-  Paper,
-  List,
-  ListItem,
-  ListItemText,
-  Collapse,
-} from "@mui/material";
+import React, { useState, forwardRef } from "react";
+import { Grid, Paper, List, ListItem, ListItemText } from "@mui/material";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
-import { TreeView, TreeItem } from "@mui/x-tree-view";
+import { TreeView } from "@mui/x-tree-view";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank"; // Corrected
 import CheckBoxIcon from "@mui/icons-material/CheckBox"; // Added
+import { useSpring, animated } from "@react-spring/web";
+import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
+import { TransitionProps } from "@mui/material/transitions";
+import { alpha, styled } from "@mui/material/styles";
+import Collapse from "@mui/material/Collapse";
+
+import {
+  TreeItem,
+  TreeItemProps,
+  treeItemClasses,
+} from "@mui/x-tree-view/TreeItem";
+
+function MinusSquare(props: SvgIconProps) {
+  return (
+    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+      {/* tslint:disable-next-line: max-line-length */}
+      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
+    </SvgIcon>
+  );
+}
+
+function PlusSquare(props: SvgIconProps) {
+  return (
+    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+      {/* tslint:disable-next-line: max-line-length */}
+      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
+    </SvgIcon>
+  );
+}
+
+function CloseSquare(props: SvgIconProps) {
+  return (
+    <SvgIcon
+      className="close"
+      fontSize="inherit"
+      style={{ width: 14, height: 14 }}
+      {...props}
+    >
+      {/* tslint:disable-next-line: max-line-length */}
+      <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
+    </SvgIcon>
+  );
+}
+
+function TransitionComponent(props: TransitionProps) {
+  const style = useSpring({
+    to: {
+      opacity: props.in ? 1 : 0,
+      transform: `translate3d(${props.in ? 0 : 20}px,0,0)`,
+    },
+  });
+
+  return (
+    <animated.div style={style}>
+      <Collapse {...props} />
+    </animated.div>
+  );
+}
 
 interface NavBarComponentProps {
   hashTable: Record<string, Record<string, Record<string, string>>>;
@@ -30,6 +81,39 @@ const NavBarComponent: React.FC<NavBarComponentProps> = ({
   onSelectMonth,
   onSelectDay,
 }) => {
+  // CustomTreeItem using forwardRef
+  const CustomTreeItem = forwardRef((props: TreeItemProps, ref) => (
+    <TreeItem {...props} TransitionComponent={TransitionComponent} ref={ref} />
+  ));
+
+  const TransitionComponent = (props) => {
+    const style = useSpring({
+      from: { opacity: 0, transform: "translate3d(20px,0,0)" },
+      to: {
+        opacity: props.in ? 1 : 0,
+        transform: `translate3d(${props.in ? 0 : 20}px,0,0)`,
+      },
+      config: { friction: 40, tension: 170 }, // Adjust for desired effect
+    });
+
+    return (
+      <animated.div style={style}>
+        <Collapse {...props} />
+      </animated.div>
+    );
+  };
+
+  // StyledTreeItem for custom styling
+  const StyledTreeItem = styled(CustomTreeItem)(({ theme }) => ({
+    [`& .${treeItemClasses.iconContainer}`]: {
+      "& .close": { opacity: 0.3 },
+    },
+    [`& .${treeItemClasses.group}`]: {
+      marginLeft: 15,
+      paddingLeft: 18,
+      borderLeft: `1px dashed ${theme.palette.text.primary}`,
+    },
+  }));
   const [expanded, setExpanded] = useState<string[]>([]);
   const [selected, setSelected] = useState<string[]>([]);
 
@@ -70,12 +154,12 @@ const NavBarComponent: React.FC<NavBarComponentProps> = ({
       const newPrefix = prefix ? `${prefix}-${key}` : key;
       if (typeof data[key] === "object") {
         return (
-          <TreeItem key={newPrefix} nodeId={newPrefix} label={key}>
+          <StyledTreeItem key={newPrefix} nodeId={newPrefix} label={key}>
             {renderTreeItems(data[key], newPrefix)}
-          </TreeItem>
+          </StyledTreeItem>
         );
       }
-      return <TreeItem key={newPrefix} nodeId={newPrefix} label={key} />;
+      return <StyledTreeItem key={newPrefix} nodeId={newPrefix} label={key} />;
     });
   };
 
@@ -147,8 +231,10 @@ const NavBarComponent: React.FC<NavBarComponentProps> = ({
         </Box>
         <TreeView
           aria-label="file system navigator"
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpandIcon={<ChevronRightIcon />}
+          defaultExpanded={["1"]}
+          defaultCollapseIcon={<MinusSquare />}
+          defaultExpandIcon={<PlusSquare />}
+          defaultEndIcon={<CloseSquare />}
           expanded={expanded}
           selected={selected}
           onNodeToggle={handleToggle}
